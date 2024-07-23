bdddb7cd797f140d3e0fec35e0c6cd8e771a97dd
Erickcod <ericksonuram@gmail.com>
2024-07-23 11:01:41 -0500

    Primera version

---
+++ server/constants.js
@@ -0,0 +1,16 @@
+const DB_USER = "mern"
+const DB_PASSWORD = "T9G0CFKU0b09dVu3"
+const DB_HOST = "web-personal.pfw2yth.mongodb.net"
+const JWT_SCRET_KEY ="lansdknaskdnaksndaksndkasndahfena"
+
+const API_VERSION ="v1"
+const IP_SERVER = "localhost"
+
+module.exports ={
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SCRET_KEY
+}
